1a5f53aa9a48a822cf913725380e80d330c36b9d
Michael Cook <cookeghost@gmail.com>
2022-04-21 13:50:40 -0700

    day2_hw

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 import os
-from sqlite3 import SQLITE_ALTER_TABLE
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Give access to the project in any OS we find ourselves in
-# Allow outside files/folders to be added to the project from 
-# base directory
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config():
     """
@@ -13,6 +11,8 @@ class Config():
     Using Environment variables where available otherwise
     create the config variable if not done already
     """
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "Its a secret."
-    SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MOIFICATIONS = False
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "Shhh... Its a secret."
+    SQLALCHEMY_DATABASE_URI= os.environ.get("DEPLOY_DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
